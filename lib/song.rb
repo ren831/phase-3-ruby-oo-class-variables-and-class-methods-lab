c3623a0af94e64c366da1d098da22ec2eddb4587
ren831 <rene83120@gmail.com>
2022-05-09 15:55:14 -0700

    commit tree d83ec848bbb6ce1a35ec6f549c706487e6334606

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(provided_name, provided_artist, provided_genre)
+    self.name = provided_name
+    self.artist = provided_artist
+    self.genre = provided_genre
+    @@count += 1
+    @@artists.push(provided_artist)
+    @@genres.push(provided_genre)
+  end
+
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    genre_count_hash = {}
+    @@genres.each do |genre|
+      if genre_count_hash.key?(genre)
+        genre_count_hash[genre] += 1
+      else
+        genre_count_hash[genre] = 1
+      end
+    end
+      genre_count_hash
+  end
+  def self.artist_count
+    @@artists.tally
+  end
+
+end
